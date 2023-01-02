d6187bd96a29b2fb0da3b2ddfe6cdc1716d97f3c
Lukas Struck <lukas.struck@pm.me>
2023-01-02 18:43:50 +0100

    add next_token method to lexer

--- calculator/lexer.py
+++ calculator/lexer.py
@@ -1,3 +1,6 @@
 class Lexer:
     def __init__(self, input_string):
         self.input_string = input_string
+
+    def next_token(self):
+        pass
--- tests/test_lexing.py
+++ tests/test_lexing.py
@@ -4,8 +4,11 @@ from calculator.lexer import Lexer
 
 
 class TestLexing(unittest.TestCase):
-    def test_initLexerWithEmptyString(self):
+    def test_initLexerWithEmptyString_nextReturnsNone(self):
         lexer = Lexer("")
+        actual = lexer.next_token()
+        expected = None
+        self.assertEqual(actual, expected)
 
 
 if __name__ == '__main__':
